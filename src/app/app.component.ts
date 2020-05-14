6249a58d63e281beae967a6a01fb42d7314f0bf8
Stefan Rosic <stefanrosic2@gmail.com>
2020-05-14 13:30:43 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular';
+  title = 'Translator tool';
+  name:string= '';
+
+  constructor(private httpClient:HttpClient){  }
+  onNameKeyUp(event:any){
+    this.name = event.target.value;
+  }
+
+  ev(){
+    var timer;
+    var timeout = 3000;
+    var get = this.httpClient;
+      clearTimeout(timer);
+        if ($('#input_word_to_translate').val) {
+            timer = setTimeout(function(){
+              get.get('https://localhost:44306/api/get')
+              .subscribe(
+                (data:any[]) => {
+                    console.log('DATA: ' + data);
+                }
+              )
+            }, timeout);
+        }
+  }
+    
+  getProfile(){
+    console.log('fffffffffffffff')
+
+  }
 }
